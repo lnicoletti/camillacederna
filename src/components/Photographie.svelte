0d973f0f50121abb538ccc81b94d6542e17bd7f4
lnicoletti <leo.nicoletti@hotmail.fr>
2022-05-23 13:00:59 +0200

    update page

---
+++ src/components/Photographie.svelte
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+
+<section>
+    <span>Mise à jour en cours, revenez un peu plus tard...</span>
+</section>
+
+<style>
+    
+section {
+    width: 50vw;
+	max-width: 500px;
+    height: 100px;
+    margin: auto;
+    text-align: center;
+}
+
+div {
+    text-align: left;
+    font-size:18px;
+	font-variation-settings: 'opsz' 144, 'slnt' 0, 'wdth' 100,'GRAD' 0;
+}
+
+.date {
+    font-variation-settings: 'opsz' 144, 'slnt' 0, 'wght' 500, 'wdth' 100,'GRAD' 150;
+    color:rgb(81, 35, 155)
+}
+
+h1 {
+    color: rgb(5, 143, 113);
+    /* text-transform: uppercase; */
+    font-size: 5em;
+    font-weight: 100;
+    font-family: 'Square Peg', cursive
+    
+}
+
+@media (max-width: 640px) {
+    section {
+        max-width: none;
+    }
+}
+</style>
